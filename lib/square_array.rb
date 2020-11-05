8dcc066cc0cf6a2c2aa8d691b0c9fae365317c40
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-05 07:32:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0 
   sq_arr = []
   while count < array.length
-  sq_arr << Math.pow (array[count])
+  sq_arr << Math.pow (array[count], 2)
   count += 1 
   end
   sq_arr
